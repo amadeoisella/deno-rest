7a74e520c10a3fa740d0e8f156b07dc51137f6e2
Amadeo Isella <aisellamk@gmail.com>
2022-08-16 23:29:55 -0300

    first commit

---
+++ main.ts
@@ -0,0 +1,10 @@
+import { Application } from "./deps.ts";
+import { productRouter } from "./routes/ProductRoutes.ts";
+
+const app = new Application();
+
+app.use(productRouter.routes());
+app.use(productRouter.allowedMethods());
+
+app.listen({ port: 3000 });
+console.log("Server on localhost:3000");
